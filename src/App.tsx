1baabc46309003fffbfd3146016a175a7f8da2fc
Joey Thompson <joeythompson1014@gmail.com>
2024-03-01 12:36:31 -0500

    Formatting of UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import './App.css'
-import { Typography } from '@mui/material'
 import StockSearch from './components/StockSearch'
 
 function App() {
 
   return (
     <div>
-      <Typography variant='h1'>Welcome to BullRun!</Typography>
       <StockSearch />
     </div>
   )
