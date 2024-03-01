efdeabd97ab2d7824b8c9d6d66519b0973f63be1
Joey Thompson <joeythompson1014@gmail.com>
2024-03-01 13:16:47 -0500

    Added AppBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import './App.css'
+import ResponsiveAppBar from './common/commonAppBar'
 import StockSearch from './components/StockSearch'
 
 function App() {
 
   return (
     <div>
-      <StockSearch />
+      <ResponsiveAppBar />
+      <div className="App" style={{ paddingTop: '50px' }}>
+        <StockSearch />
+      </div>
     </div>
   )
 }
